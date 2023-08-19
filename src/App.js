64cde78007f49f679a2d597c35d9cb52bf2a8efb
Prateek Sharma <prateek0775.cse19@chitkara.edu.in>
2023-08-18 23:17:02 -0700

    Deployment Fix

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     // <Router>
       <Grid>
-        
+        <Home></Home>
         {/* <Routes>
           <Route path='/' element = {<Home></Home>} ></Route>
           <Route path='https://prateeks7.github.io/AfterImage/Create' element = {<Create></Create>} ></Route>
