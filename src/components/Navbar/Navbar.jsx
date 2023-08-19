f649940fe783c18b9cfaa7398900db620793d053
Prateek Sharma <prateek0775.cse19@chitkara.edu.in>
2023-08-18 22:53:20 -0700

    Deployment Fix

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,7 +8,7 @@ function Navbar(props)
         <div className='d-flex justify-content-between border-bottom sticky-top bg-black'>
             <Link to='/'>
                 <div className='p-3'>
-                    <img src="https://static01.nyt.com/images/2022/09/01/business/00roose-1/merlin_212276709_3104aef5-3dc4-4288-bb44-9e5624db0b37-superJumbo.jpg" alt='logo' className='img-fluid logoSize' />
+                    <img src={logo} alt='logo' className='img-fluid logoSize' />
                 </div>
             </Link>
             <Link to={props.children === 'Create' ? '/Create' : '/'}>
