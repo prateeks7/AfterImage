85d8d78eaefcdb7bb12dc493f397d7300331711d
Prateek Sharma <prateek0775.cse19@chitkara.edu.in>
2023-08-22 23:04:27 -0700

    Version 1 Server Client

--- client/src/App.js
+++ client/src/App.js
@@ -10,8 +10,8 @@ function App() {
     <Router>
       <Grid>
         <Routes>
-          <Route path='/' element = {<Home></Home>} ></Route>
-          <Route path='/Create' element = {<Create></Create>} ></Route>
+          <Route path='/AfterImage/' element = {<Home></Home>} ></Route>
+          <Route path='/AfterImage/Create' element = {<Create></Create>} ></Route>
         </Routes>
       </Grid>
     </Router>
