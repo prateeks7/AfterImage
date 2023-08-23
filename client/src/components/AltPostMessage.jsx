5b0b013749843750540f1d15920fd1d3d1faec20
Prateek Sharma <prateek0775.cse19@chitkara.edu.in>
2023-08-23 00:05:51 -0700

    Version 1 Server Client

--- client/src/components/AltPostMessage.jsx
+++ client/src/components/AltPostMessage.jsx
@@ -1,14 +1,15 @@
 import './styles/PageD.css';
+import './styles/AltPostMessage.css';
 import loader from '../assets/loading.svg';
 function AltPostMessage(props)
 {
     return(
-        <div className='col-sm-6 col-md-6 col-xl-4 col-xxl-6 p-3'>
-            <div className="card cardCss">
+        <div className='col-sm-6 col-md-6 col-xl-4 col-xxl-6 p-3 '>
+            <div className="card cardCss d-flex justify-content-center altPostMessageCss">
                 {   
                     props.type === "loading" ?
-                    <p className="pageD">No Posts Yet</p> :
-                    <img src={loader} className="gImageCss"></img>
+                    <img src={loader} className="altLoaderCss"></img>:
+                    <p className="pageD">No Posts Yet</p>
                 }
             </div>
         </div>
